cc29553c89f0b3a66e92b916a99a84ba8a90b6f4
bnshr <bmandal@redhat.com>
2023-05-05 17:40:44 +0200

    Initial

--- schema.sql
+++ schema.sql
@@ -2,19 +2,19 @@ create database cnf;
 use cnf;
 
 create table claim (
-  id int not null,
-  cnf_version varchar(40) not null, 
-  created_by  varchar(40) not null,
-  upload_time DATETIME not null,
-  partner_name varchar(40),
+  id int not null AUTO_INCREMENT,
+  cnf_version varchar(50) not null, 
+  created_by  varchar(50) not null,
+  upload_time datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  partner_name varchar(50),
   primary key (id)
 );
 
 create table claim_result (
-  id int not null,
+  id int not null AUTO_INCREMENT,
   claim_id int not null,
-  suite_name varchar(50),
-  test_id varchar(100),
+  suite_name varchar(255),
+  test_id varchar(255),
   test_status varchar(10) not null,
   primary key (id),
   foreign key (claim_id) references claim(id)
